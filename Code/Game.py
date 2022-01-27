e09332a73202cb6f05f1925b9ca34e9d0965c506
Sasha <user@gmail.com>
2022-01-27 14:55:49 +0300

    100%

--- Code/Game.py
+++ Code/Game.py
@@ -49,9 +49,7 @@ class Button_s:
 
 
 class Button_n:
-    def __init__(self, text, color, x, y, font, screen, h, w):
-        # height  # высота
-        # width   ширина
+    def __init__(self, text, color, x, y, font, screen, h, w, music):
         self.width = w
         self.height = h
         pygame.font.init()
@@ -61,53 +59,16 @@ class Button_n:
         mouse = pygame.mouse.get_pos()
         click = pygame.mouse.get_pressed()
         self.meteorit_group = []
-
         if x < mouse[0] < x + self.width and y < mouse[1] < y + self.height:
             pygame.draw.rect(screen, (21, 20, 24), (x - 10, y, self.width, self.height))
-            if click[0] == 1 and y == 200:
-                WINDOWWIDTH = 1200
-                WINDOWHEIGHT = 700
-                mainClock = pygame.time.Clock()
-                windowSurface = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT))
-                pygame.display.set_caption('Dodger')
-                pygame.mouse.set_visible(True)
-                while True:
-                    Surphase(windowSurface)
-                    boat_1 = pygame.image.load(r'Photo/boat_2.png')
-                    boat_1 = pygame.transform.scale(boat_1, (300, 300))
-                    boat_1.set_colorkey((255, 255, 255))
-                    windowSurface.blit(boat_1, (100, 100))
-                    boat_2 = pygame.image.load(r'Photo/pixil-frame-0 (2).png')
-                    boat_2 = pygame.transform.scale(boat_2, (300, 300))
-                    boat_2.set_colorkey((255, 255, 255))
-                    windowSurface.blit(boat_2, (500, 100))
-                    pygame.display.update()
-                    if 100 < mouse[0] < 100 + 300 and 100 < mouse[1] < 100 + 300:
-                        pygame.draw.rect(screen, (21, 20, 24), (100 - 10, 100, 300, 300))
-                        if click[0] == 1 and x == 100:
-                            cast_boat(r'Photo/boat_2.png')
-                            for event in pygame.event.get():
-                                if event.type == QUIT:
-                                    terminate()
-                                if event.type == KEYDOWN:
-                                    if event.key == K_ESCAPE:  # pressing escape quits
-                                        WINDOWWIDTH = 1200
-                                        WINDOWHEIGHT = 700
-                                        mainClock = pygame.time.Clock()
-                                        windowSurface = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT))
-                                        pygame.display.set_caption('Dodger')
-                                        pygame.mouse.set_visible(True)
-
-                                        pygame.display.update()
-                                        while pygame.event.wait().type != pygame.QUIT:
-                                            #music_t.stop()
-                                            WaitForPlayerToPressKey(1)
-                                    return
-
-            elif click[0] == 1 and y == 300:
-                cast_met()
-            elif click[0] == 1 and y == 400:
-                cast_backgr()
+            if click[0] == 1 and x == 100:
+                music.stop()
+                cast_boat(r'Photo/boat_2.png')
+                WaitForPlayerToPressKey(1)
+            elif click[0] == 1 and x == 500:
+                music.stop()
+                cast_boat(r'Photo/pixil-frame-0 (2).png')
+                WaitForPlayerToPressKey(1)
         pygame.font.init()
         text_r = font.render(text, True, color)
         screen.blit(text_r, (x, y))
@@ -207,7 +168,7 @@ def game_go():
         moveLeft = moveRight = moveUp = moveDown = False
         reverseCheat = slowCheat = False
         baddieAddCounter = 0
-        #pygame.mixer.music.play(-1, 0.0)
+        # pygame.mixer.music.play(-1, 0.0)
         while True:
             score += 1
             for event in pygame.event.get():
@@ -406,9 +367,16 @@ def castomize():
     while True:
         Surphase(windowSurface)
         Button('Нажмите esc для возвращения в главное меню', (255, 255, 255), 0, 0, 40, windowSurface)
-        Button_n('Кастомизировать корабль', (255, 255, 255), 400, 200, 40, windowSurface, 60, 415)
-        Button_n('Кастомизировать метеориты', (255, 255, 255), 400, 300, 40, windowSurface, 60, 460)
-        Button_n('Кастомизировать задний фон', (255, 255, 255), 400, 400, 40, windowSurface, 60, 470)
+        boat_1 = pygame.image.load(r'Photo/boat_2.png')
+        boat_1 = pygame.transform.scale(boat_1, (300, 300))
+        boat_1.set_colorkey((255, 255, 255))
+        windowSurface.blit(boat_1, (100, 100))
+        Button_n('выбрать', (255, 255, 255), 100, 420, 40, windowSurface, 50, 200, music)
+        boat_2 = pygame.image.load(r'Photo/pixil-frame-0 (2).png')
+        boat_2 = pygame.transform.scale(boat_2, (300, 300))
+        boat_2.set_colorkey((255, 255, 255))
+        windowSurface.blit(boat_2, (500, 100))
+        Button_n('выбрать', (255, 255, 255), 500, 420, 40, windowSurface, 50, 200, music)
         pygame.display.update()
         for event in pygame.event.get():
             if event.type == QUIT:
@@ -461,7 +429,6 @@ timeout = 60.0
 clock = pygame.time.Clock()
 FPS = 120
 boat = r'Photo/pixil-frame-0 (2).png'
-#boat = r'Photo/boat_2.png'
 
 WINDOWWIDTH = 1200
 WINDOWHEIGHT = 700
